b4d2709d7b9802ecd84452fe00ae4b27cb146d81
Ahmed Al-Zubidy <ahmed.alzubidy@itential.com>
2020-09-18 14:16:03 -0400

    Added drawer

--- App.js
+++ App.js
@@ -1,25 +1,38 @@
 import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import HomeScreen from './HomeScreen';
+import Education from './Education';
+import testImage from './img/landscape-lowres.jpg';
 
 const Stack = createStackNavigator();
+
+const Drawer = createDrawerNavigator();
 
 const MyStack = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Hi There!"
+      <Drawer.Navigator initialRouteName="Home" drawerType="front" edgeWidth={100} minSwipeDistance drawerStyle={styles.drawer}
+      drawerContentOptions={{
+        activeTintColor: 'black',
+        activeBackgroundColor: '#fbdbc6',
+        inactiveTintColor: 'black',
+        }}
+      >
+        <Drawer.Screen
+          name="Home"
           component={HomeScreen}
           options={{
+            title: 'Hey There!',
             headerStyle: {
               backgroundColor: '#f7ae80',
               opacity: 1,
             },
+            drawerIcon: ({ focused, size }) => (<Image source={testImage} style={[focused ? styles.drawerActive : styles.drawerInActive, { height: size, width: size }]}/>),
             headerTintColor: 'black',
             headerTitleStyle: {
               // fontSize: 28
@@ -27,8 +40,21 @@ const MyStack = () => {
             }
           }}
         />
-        
-      </Stack.Navigator>
+        <Drawer.Screen
+          name="Education"
+          component={Education}
+          options={{
+            headerStyle: {
+              backgroundColor: '#f7ae80',
+              opacity: 1,
+            },
+            headerTintColor: 'black',
+            headerTitleStyle: {
+              alignSelf: 'center'
+            }
+          }}
+        />
+      </Drawer.Navigator>
     </NavigationContainer>
   );
 };
@@ -36,11 +62,11 @@ const MyStack = () => {
 export default MyStack;
 
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
+const styles = StyleSheet.create({
+  drawer: {
+    backgroundColor: '#f7ae80',
+    // width: 240,
+    flex: 1,
+    padding: 1,
+  },
+});
---
+++ Education.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, ImageBackground, Image, SafeAreaView, ScrollView } from 'react-native';
+import { vw, vh, vmin, vmax } from 'react-native-expo-viewport-units';
+
+class Education extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return (
+            <View>
+                <Text>Hello</Text>
+            </View>
+        );
+    }
+}
+ 
+export default Education;
